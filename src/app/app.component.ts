496247bf4a5f070f682cedaa2120d2f7330d468a
taty2010 <shydalys123@gmail.com>
2022-08-02 16:27:56 -0500

    imported styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../demo-styling.scss']
 })
 export class AppComponent {
   title = 'angular-quickstart';
