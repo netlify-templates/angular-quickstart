6fa0ba23d0cc867d33d9b230c8bda16adc0e3893
Tatyana <43764894+taty2010@users.noreply.github.com>
2022-08-09 13:24:18 -0500

    Design updates (#35)
    * updated main text, added quickstart title and link to repo + deploy
    
    * removed body margin and customized other elements to better match original
    
    * imported styling
    
    * update blob color
    
    * updated tests
    
    * removed duplicate link
    
    * removed sass and changed files to css
    
    * uninstalled sass
    
    * moved description above CTA
    
    * updated readme with styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../demo-styling.css']
 })
 export class AppComponent {
   title = 'angular-quickstart';
