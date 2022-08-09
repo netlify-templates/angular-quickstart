b8f82cc22a3b48d7ec2e87c006fe98d16f7d0436
taty2010 <shydalys123@gmail.com>
2022-08-09 12:51:30 -0500

    removed sass and changed files to css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.scss']
+  styleUrls: ['../demo-styling.css']
 })
 export class AppComponent {
   title = 'angular-quickstart';
