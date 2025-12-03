e5df36a18687cca77791a321aa5f58fa972199e1
OutdoorScientist <78631035+OutdoorScientist@users.noreply.github.com>
2025-12-03 13:42:19 -0600

    car repair shop initial update;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  styleUrls: ['./app.component.css']
+  // styleUrls: ['../demo-styling.css']
 })
 export class AppComponent {
   title = 'angular-quickstart';
